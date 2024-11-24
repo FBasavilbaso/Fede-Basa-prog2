1dc0c9c6328118419bbda394ddd7e997b71b2655
FBasavilbaso <fedebasa05@gmail.com>
2024-11-24 00:45:07 -0300

    BCRYPTJS

--- proyectoProg2/controllers/usersControllers.js
+++ proyectoProg2/controllers/usersControllers.js
@@ -1,9 +1,5 @@
 const db = require("../database/models");
-<<<<<<< Updated upstream
-const bcryptjs = require('bcryptjs');
-=======
 const bcryptjs = require("bcryptjs");
->>>>>>> Stashed changes
 const op = db.Sequelize.Op;
 const users = db.User;
 
